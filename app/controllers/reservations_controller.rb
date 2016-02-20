42e6d79df6f7404d67f927eacc87424fb1a8ab38
aenser <aaronenser1@gmail.com>
2016-02-20 14:53:32 -0700

    Working on hotel.name to show

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -35,7 +35,7 @@ class ReservationsController < ApplicationController
   private 
 
   def reservation_params
-    params.require(:reservation).permit(:num_nights, :num_guests, :check_in, :hotel_id)
+    params.require(:reservation).permit(:num_nights, :num_guests, :check_in)
   end
 
   # def logged_in
